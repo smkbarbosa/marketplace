119f190344287df4b94bb8f866dc99c879ffb262
Samuel Barbosa <contato@smkbarbosa.eti.br>
2020-03-02 16:25:09 -0300

    entendendo o eloquent

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,14 +11,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('users')->insert(
-            [
-                'name' => 'Administrator',
-                'email' => 'admin@admin.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => 'akakdsdsdsdsdsd',
-            ]
-        );
+//        \DB::table('users')->insert(
+//            [
+//                'name' => 'Administrator',
+//                'email' => 'admin@admin.com',
+//                'email_verified_at' => now(),
+//                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+//                'remember_token' => 'akakdsdsdsdsdsd',
+//            ]
+//        );
+        factory(\App\User::class, 40)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,27 @@ Route::get('/model', function() {
     //$products  = \App\Product::all();
 
     //$user = new \App\User();
-    $user = \App\User::find(1);
-    $user->name = 'Administrador';
-    /*$user->email = 'email@teste.com';
-    $user->password = bcrypt('12345678');*/
-
-    $user->save();
+//    $user = \App\User::find(1);
+//    $user->name = 'Administrador';
+//    /*$user->email = 'email@teste.com';
+//    $user->password = bcrypt('12345678');*/
+//
+//    $user->save();
 
     // return $products;
+//    return \App\User::all(); //--> Retorna todos os usuários
+//    return \App\User::find(3); //--> retorna usuários por id
+//    return \App\User::where('name', 'Leilani Kutch')->get(); // --> equivale a: select * from users where name = 'Leilani Kutch'
+//    return \App\User::paginate(10); // --> retorna dados paginados
+
+    /* Mass Assignment - Atribuição em massa */
+
+    $user = \App\User::create([
+        'name' => 'Samuel Barbosa',
+        'email' => 'teste@smkbarbosa',
+        'password' => bcrypt('teste')
+    ]);
+
     return \App\User::all();
 });
+
