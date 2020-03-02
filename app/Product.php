51acf8eb0880c56c752b8eeb3fa3ea162f0ac0ca
Samuel Barbosa <contato@smkbarbosa.eti.br>
2020-03-02 16:41:33 -0300

    cria relação n:n entre categorias e produtos

--- app/Product.php
+++ app/Product.php
@@ -11,4 +11,9 @@ class Product extends Model
     {
         return $this->belongsTo(Store::class);
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
