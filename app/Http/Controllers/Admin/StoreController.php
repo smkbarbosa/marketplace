010623d49ded24829c478d48d9543a6ccf4ce3cc
Samuel Barbosa <contato@smkbarbosa.eti.br>
2020-03-03 13:51:58 -0300

    exibindo e paginando as lojas

---
+++ app/Http/Controllers/Admin/StoreController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function index()
+    {
+        $stores = \App\Store::paginate(10);
+
+        return view('admin.stores.index', compact('stores'));
+    }
+}
---
+++ resources/views/admin/stores/index.blade.php
@@ -0,0 +1,19 @@
+<table>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Loja</th>
+        <th>Ações</th>
+    </tr>
+    </thead>
+    <tbody>
+        @foreach($stores as $store)
+            <tr>
+                <td>{{$store->id}}</td>
+                <td>{{$store->name}}</td>
+                <td></td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+{{$stores->links()}}
