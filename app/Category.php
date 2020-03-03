693ce66de331bb99da1c8a527ba6dc5bb7e1769c
Samuel Barbosa <contato@smkbarbosa.eti.br>
2020-03-03 00:21:43 -0300

    cria factorys para loja, produtos e categorias

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'description', 'slug'];
+
     public function products()
     {
         return $this->belongsToMany(Product::class);
---
+++ database/factories/StoreFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->sentence,
+        'phone' => $faker->phoneNumber,
+        'mobile_phone' => $faker->mobile_phone,
+        'slug' => $faker->slug,
+    ];
+});
