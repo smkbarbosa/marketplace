483898648ddee2d14a2cfd15de9aac33101120c5
Samuel Barbosa <contato@smkbarbosa.eti.br>
2020-03-05 16:31:14 -0300

    add nomes das rotas

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -4,7 +4,7 @@
     <h1>Editar Loja</h1>
 
 
-    <form action="/admin/stores/store" method="post">
+    <form action="{{route('admin.stores.store')}}" method="post">
         <input type="hidden" name="_token" value="{{csrf_token()}}">
         <div class="form-group">
             <label for="name">Nome Loja </label>
--- routes/web.php
+++ routes/web.php
@@ -95,14 +95,14 @@ Route::get('/model', function() {
     return \App\Product::find(1);
 });
 
-Route::prefix('admin')->namespace('Admin')->group(function(){
-    Route::prefix('stores')->group(function (){
-        Route::get('/', 'StoreController@index');
-        Route::get('/create', 'StoreController@create');
-        Route::post('/store', 'StoreController@store');;
-        Route::get('/{store}/edit', 'StoreController@edit');
-        Route::post('/update/{store}', 'StoreController@update');
-        Route::get('/destroy/{store}', 'StoreController@destroy');
+Route::prefix('admin')->name('admin')->namespace('Admin')->group(function(){
+    Route::prefix('stores')->name('stores')->group(function (){
+        Route::get('/', 'StoreController@index')->name('index');
+        Route::get('/create', 'StoreController@create')->name('create');
+        Route::post('/store', 'StoreController@store')->name('store');;
+        Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+        Route::post('/update/{store}', 'StoreController@update')->name('update');
+        Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
     });
 
 });
